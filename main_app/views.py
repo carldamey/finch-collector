0b628656a65be41e43e6adb8a6b80da299f1b881
carldamey <carldamey@gmail.com>
2023-12-11 23:27:21 -0800

    finishes first deliverable by implementing finch indexing

--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render
 
+finches = [
+  {'name': 'Birdtram', 'type': 'Saffron', 'description': 'he fly', 'age': 8},
+  {'name': 'Birdley', 'type': 'Society', 'description': 'society lives in a him', 'age': 3},
+  {'name': 'Birdolemew', 'type': 'Star', 'description': 'he squawk', 'age': 5},
+]
+
 def home(request):
   return render(request, 'home.html')
 
 def about(request):
   return render(request, 'about.html')
+
+def finches_index(request):
+  return render(request, 'finches/index.html', {
+    'finches': finches
+  })
