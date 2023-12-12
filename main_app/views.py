046c1f31a89599919ef37f07e81a3945c323a315
carldamey <carldamey@gmail.com>
2023-12-11 23:01:22 -0800

    implements base html

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
-# Define the home view
 def home(request):
-  # Include an .html file extension - unlike when rendering EJS templates
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
