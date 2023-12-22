908b7090d495915cabca46c103dd2188ea69230c
carldamey <carldamey@gmail.com>
2023-12-21 16:50:20 -0800

    updates finch model

--- main_app/models.py
+++ main_app/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
 class Finch(models.Model):
   name = models.CharField(max_length=100)
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField()
-
+  class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+      max_length=1,
+      choices=MEALS,
+      default=MEALS[0][0]
+  )
   def __str__(self):
     return f"{self.name} ({self.id})"
   
